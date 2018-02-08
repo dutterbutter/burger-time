13abefdd2cb5597e0eb018b5fb804b354a318e95
Dustin Brickwood <dustinbrickwood204@gmail.com>
2018-02-07 20:59:41 -0500

    finished build controls, modal next

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,9 @@
 import React from 'react';
+
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 
-const layout = (props) => (
+const layout = ( props ) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
